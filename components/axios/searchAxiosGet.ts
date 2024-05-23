da7d5345c7d6b13fb54467d27de8be6abb0570d2
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-05-23 00:22:31 -0300

    add query custom from input user

--- components/axios/searchAxiosGet.ts
+++ components/axios/searchAxiosGet.ts
@@ -1,19 +1,19 @@
 import axios, { AxiosResponse } from 'axios';
 
-export async function searchAxiosGet(
-  rota?: string,
+export async function searchAxiosGetWords(
+  Query?: string | string[],
   options?: { query?: string; mes?: string; ano?: string },
 ): Promise<AxiosResponse> {
-  return new Promise(async (resolver, reject) => {
+  return new Promise(async (resolve, reject) => {
     try {
       const data: AxiosResponse<any> = await axios.get(
-        `http://192.168.100.133:4002/word/example`,
+        `http://192.168.100.133:4002/word/${Query}`,
       );
-      // setTimeout(() => {}, 5000);
-      resolver(data);
-    } catch (err) {
-      reject({ error: err });
+      resolve(data); // Resolve com a resposta se houver dados
+    } catch (error) {
+      reject(error); // Rejeita a promessa em caso de erro
     }
   });
 }
+
 // https://run.mocky.io/v3/a44a54fa-8fc5-4796-9086-16493c5259df
