852bebf7627709481a6aad3ba2dab9ac32619cb1
ErickSousa312 <58636303+ErickSousa312@users.noreply.github.com>
2024-05-25 11:27:55 -0300

    route updated and created route searchById of any route

--- components/axios/searchById.ts
+++ components/axios/searchById.ts
@@ -1,14 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
 import { ProgressTransitionManager } from 'react-native-reanimated/lib/typescript/reanimated2/layoutReanimation';
 
-export async function searchByRoute(
-  route: string | string[],
-  options?: { query?: string; mes?: string; ano?: string },
+export async function searchById(
+  rota: string,
+  id?: string | string[],
 ): Promise<AxiosResponse> {
   return new Promise(async (resolve, reject) => {
     try {
       const data: AxiosResponse<any> = await axios.get(
-        `http://192.168.100.133:4002/${route}`,
+        `http://192.168.100.133:4002/${rota}/${id}`,
       );
       resolve(data);
     } catch (error) {
